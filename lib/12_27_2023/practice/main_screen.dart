7658aa629cc5d9c0119b795401b0b13287fd5317
slee3593 <slee3593@gatech.edu>
2023-12-27 15:26:20 +0900

    add: 검색창 위젯, 모델 클래스

--- lib/12_27_2023/practice/main_screen.dart
+++ lib/12_27_2023/practice/main_screen.dart
@@ -10,6 +10,41 @@ class MainScreen extends StatefulWidget {
 class _MainScreenState extends State<MainScreen> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: SafeArea(
+          child: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          children: [
+            TextField(
+              decoration: InputDecoration(
+                focusedBorder: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(10.0),
+                ),
+                hintText: 'Search image',
+                suffixIcon: const Icon(
+                  Icons.search,
+                  color: Color(0xFF4FB6B2),
+                ),
+              ),
+            ),
+
+            Expanded(
+              child: GridView.builder(
+                itemCount: 10,
+                gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                  crossAxisCount: 2,
+                  crossAxisSpacing: 32,
+                  mainAxisSpacing: 32,
+                ),
+                itemBuilder: (context, index) {
+
+                },
+              ),
+            )
+          ],
+        ),
+      )),
+    );
   }
 }
